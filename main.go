f938f7730eb61a4c1f133188030f2fa4fae98b9d
Ricky McLaren <ricky.mclaren@cybg.com>
2020-02-26 19:59:34 +0000

    Add interface

--- main.go
+++ main.go
@@ -36,26 +36,30 @@ const (
 	ZapfDingbats
 )
 
-var objectIDCounter = 0
-
-func nextObjectID() int {
-	objectIDCounter++
-	return objectIDCounter
+// PdfObjectWriter is an interface that all objects implement to allow us to treat the PDF as a list of objects
+// and easily write it out.
+type PdfObjectWriter interface {
+	setID(id int)
+	bytes() []byte
 }
 
 // PdfObject is the base object that has an id and a reference to the containing document.
+// It implements PdfObjectWriter
 type PdfObject struct {
-	objectID int
+	id       int
 	document *PdfDocument
 }
 
-func (pdfObject PdfObject) objectRef() string {
-	return fmt.Sprintf("%v 0 R", pdfObject.objectID)
+func (o *PdfObject) setID(id int) {
+	o.id = id
 }
 
-func (pdfObject PdfObject) bytes() []byte {
-	var buf bytes.Buffer
-	return buf.Bytes()
+func (o PdfObject) objectRef() string {
+	return fmt.Sprintf("%v 0 R", o.id)
+}
+
+func (o PdfObject) bytes() []byte {
+	panic(fmt.Sprintf("TODO - write bytes method for %T", o))
 }
 
 // PdfFont stores the details of one of the 14 base fonts
@@ -97,18 +101,17 @@ func NewFont(name string, font int) PdfFont {
 	default:
 		panic("Invalid font " + string(font))
 	}
-	result.objectID = nextObjectID()
 	return result
 }
 
-func (pdfFont PdfFont) bytes() []byte {
+func (f PdfFont) bytes() []byte {
 	var buf bytes.Buffer
-	fmt.Fprintf(&buf, "%v 0 obj\r\n", pdfFont.objectID)
+	fmt.Fprintf(&buf, "%v 0 obj\r\n", f.id)
 	fmt.Fprintf(&buf, "<<\r\n")
 	fmt.Fprintf(&buf, "/Type /Font \r\n")
-	fmt.Fprintf(&buf, "/Subtype %v \r\n", pdfFont.subtype)
-	fmt.Fprintf(&buf, "/Name %v \r\n", pdfFont.name)
-	fmt.Fprintf(&buf, "/BaseFont %v \r\n", pdfFont.baseFont)
+	fmt.Fprintf(&buf, "/Subtype %v \r\n", f.subtype)
+	fmt.Fprintf(&buf, "/Name %v \r\n", f.name)
+	fmt.Fprintf(&buf, "/BaseFont %v \r\n", f.baseFont)
 	fmt.Fprintf(&buf, ">>\r\n")
 	fmt.Fprintf(&buf, "endobj\r\n")
 	return buf.Bytes()
@@ -122,15 +125,15 @@ type PdfImage struct {
 	height int
 }
 
-func (pdfImage PdfImage) bytes() []byte {
+func (i PdfImage) bytes() []byte {
 	var buf bytes.Buffer
-	fmt.Fprintf(&buf, "%v 0 obj\r\n", pdfImage.objectID)
+	fmt.Fprintf(&buf, "%v 0 obj\r\n", i.id)
 	fmt.Fprintf(&buf, "<<\r\n")
 	fmt.Fprintf(&buf, "/Type /XObject\r\n")
 	fmt.Fprintf(&buf, "/Subtype /Image\r\n")
-	fmt.Fprintf(&buf, "/Name %v\r\n", pdfImage.name)
-	fmt.Fprintf(&buf, "/Width %v\r\n", pdfImage.width)
-	fmt.Fprintf(&buf, "/Height %v\r\n", pdfImage.height)
+	fmt.Fprintf(&buf, "/Name %v\r\n", i.name)
+	fmt.Fprintf(&buf, "/Width %v\r\n", i.width)
+	fmt.Fprintf(&buf, "/Height %v\r\n", i.height)
 	fmt.Fprintf(&buf, "/BitsPerComponent 8\r\n")
 	fmt.Fprintf(&buf, "/ColorSpace /DeviceRGB\r\n")
 	fmt.Fprintf(&buf, "/Filter [/FlateDecode]\r\n")
@@ -150,10 +153,10 @@ type PdfPageContent struct {
 	text, lines, graphics string
 }
 
-func (content *PdfPageContent) bytes() []byte {
+func (c *PdfPageContent) bytes() []byte {
 	var buf bytes.Buffer
-	stream := "BT\r\n" + content.text + "\r\nET\r\n" + content.lines + "S\r\n" + content.graphics
-	fmt.Fprintf(&buf, "%v 0 obj\r\n", content.objectID)
+	stream := "BT\r\n" + c.text + "\r\nET\r\n" + c.lines + "S\r\n" + c.graphics
+	fmt.Fprintf(&buf, "%v 0 obj\r\n", c.id)
 	fmt.Fprintf(&buf, "<<\r\n")
 	fmt.Fprintf(&buf, "/Length %v\r\n", len(stream))
 	fmt.Fprintf(&buf, ">>\r\n")
@@ -176,31 +179,44 @@ type PdfPage struct {
 	topMargin, bottomMargin int
 }
 
-func (page *PdfPage) outputText(text string) {
+func (p *PdfPage) outputText(text string) {
 	var sb strings.Builder
 	for _, c := range text {
 		if c == '(' {
-			sb.WriteString("\\(")
+			sb.WriteString(`\(`)
 		} else if c == ')' {
-			sb.WriteString("\\)")
+			sb.WriteString(`\)`)
 		} else if c == '\\' {
-			sb.WriteString("\\\\)")
+			sb.WriteString(`\\`)
 		} else {
 			sb.WriteRune(c)
 		}
 	}
-	page.content.text += fmt.Sprintf("1 0 0 1 %v %v Tm\r\n", page.leftMargin+page.x, page.height-page.topMargin-page.y-page.fontSize)
-	page.content.text += fmt.Sprintf("(%s) Tj\r\n", sb.String())
+	p.content.text += fmt.Sprintf("1 0 0 1 %v %v Tm\r\n",
+		p.leftMargin+p.x,
+		p.height-p.topMargin-p.y-p.fontSize)
+	p.content.text += fmt.Sprintf("(%s) Tj\r\n", sb.String())
 }
 
-func (pdfPage PdfPage) bytes() []byte {
+func (p *PdfPage) print(text string) {
+	p.outputText(text)
+	p.x += len(text) * p.fontSize
+}
+
+func (p *PdfPage) println(text string) {
+	p.outputText(text)
+	p.x = 0
+	p.y += p.fontSize
+}
+
+func (p PdfPage) bytes() []byte {
 	var buf bytes.Buffer
-	fmt.Fprintf(&buf, "%v 0 obj\r\n", pdfPage.objectID)
+	fmt.Fprintf(&buf, "%v 0 obj\r\n", p.id)
 	fmt.Fprintf(&buf, "<<\r\n")
 	fmt.Fprintf(&buf, "/Type /Page\r\n")
-	fmt.Fprintf(&buf, "/Parent %v\r\n", pdfPage.parent.objectRef())
-	fmt.Fprintf(&buf, "/Resources %v\r\n", pdfPage.document.resources.objectRef())
-	fmt.Fprintf(&buf, "/Contents %v\r\n", pdfPage.content.objectRef())
+	fmt.Fprintf(&buf, "/Parent %v\r\n", p.parent.objectRef())
+	fmt.Fprintf(&buf, "/Resources %v\r\n", p.document.resources.objectRef())
+	fmt.Fprintf(&buf, "/Contents %v\r\n", p.content.objectRef())
 	fmt.Fprintf(&buf, ">>\r\n")
 	fmt.Fprintf(&buf, "endobj\r\n")
 	return buf.Bytes()
@@ -209,18 +225,18 @@ func (pdfPage PdfPage) bytes() []byte {
 // PdfPages represents the list of pages
 type PdfPages struct {
 	PdfObject
-	pages []PdfPage
+	pages []*PdfPage
 }
 
-func (pdfPages PdfPages) bytes() []byte {
+func (p PdfPages) bytes() []byte {
 	var buf bytes.Buffer
-	fmt.Fprintf(&buf, "%v 0 obj\r\n", pdfPages.objectID)
+	fmt.Fprintf(&buf, "%v 0 obj\r\n", p.id)
 	fmt.Fprintf(&buf, "<<\r\n")
 	fmt.Fprintf(&buf, "/Type /Pages\r\n")
 	fmt.Fprintf(&buf, "/MediaBox [ 0 0 595 842 ]\r\n")
-	fmt.Fprintf(&buf, "/Count %v\r\n", len(pdfPages.pages))
+	fmt.Fprintf(&buf, "/Count %v\r\n", len(p.pages))
 	fmt.Fprintf(&buf, "/Kids [ ")
-	for _, page := range pdfPages.pages {
+	for _, page := range p.pages {
 		fmt.Fprintf(&buf, page.objectRef()+" ")
 	}
 	fmt.Fprintf(&buf, "]\r\n")
@@ -234,9 +250,9 @@ type PdfOutlines struct {
 	PdfObject
 }
 
-func (pdfOutlines PdfOutlines) bytes() []byte {
+func (o PdfOutlines) bytes() []byte {
 	var buf bytes.Buffer
-	fmt.Fprintf(&buf, "%v 0 obj\r\n", pdfOutlines.objectID)
+	fmt.Fprintf(&buf, "%v 0 obj\r\n", o.id)
 	fmt.Fprintf(&buf, "<<\r\n")
 	fmt.Fprintf(&buf, "/Type /Outlines\r\n")
 	fmt.Fprintf(&buf, "/Count 0\r\n") // TODO : Add outlines
@@ -252,13 +268,13 @@ type PdfCatalog struct {
 	pdfPages *PdfPages
 }
 
-func (pdfCatalog PdfCatalog) bytes() []byte {
+func (c PdfCatalog) bytes() []byte {
 	var buf bytes.Buffer
-	fmt.Fprintf(&buf, "%v 0 obj\r\n", pdfCatalog.objectID)
+	fmt.Fprintf(&buf, "%v 0 obj\r\n", c.id)
 	fmt.Fprintf(&buf, "<<\r\n")
 	fmt.Fprintf(&buf, "/Type /Catalog \r\n")
-	fmt.Fprintf(&buf, "/Outlines %v\r\n", pdfCatalog.outlines.objectRef())
-	fmt.Fprintf(&buf, "/Pages %v\r\n", pdfCatalog.pdfPages.objectRef())
+	fmt.Fprintf(&buf, "/Outlines %v\r\n", c.outlines.objectRef())
+	fmt.Fprintf(&buf, "/Pages %v\r\n", c.pdfPages.objectRef())
 	fmt.Fprintf(&buf, ">>\r\n")
 	fmt.Fprintf(&buf, "endobj\r\n")
 	return buf.Bytes()
@@ -267,36 +283,36 @@ func (pdfCatalog PdfCatalog) bytes() []byte {
 // PdfResources represents the images and fonts for the document
 type PdfResources struct {
 	PdfObject
-	fonts  []PdfFont
-	images []PdfImage
+	fonts  []*PdfFont
+	images []*PdfImage
 }
 
-func (pdfResources PdfResources) bytes() []byte {
+func (r PdfResources) bytes() []byte {
 	var buf bytes.Buffer
 	procset := "[ /PDF "
-	if len(pdfResources.fonts) > 0 {
+	if len(r.fonts) > 0 {
 		procset += "/Text "
 	}
-	if len(pdfResources.images) > 0 {
+	if len(r.images) > 0 {
 		procset += "/ImageB "
 	}
 	procset += "]"
 
-	fmt.Fprintf(&buf, "%v 0 obj\r\n", pdfResources.objectID)
+	fmt.Fprintf(&buf, "%v 0 obj\r\n", r.id)
 	fmt.Fprintf(&buf, "<<\r\n")
 	fmt.Fprintf(&buf, "/Procset %v\r\n", procset)
 
-	if len(pdfResources.fonts) > 0 {
+	if len(r.fonts) > 0 {
 		fmt.Fprintf(&buf, "/Font << ")
-		for _, font := range pdfResources.fonts {
+		for _, font := range r.fonts {
 			fmt.Fprintf(&buf, "%v %v ", font.name, font.objectRef())
 		}
 		fmt.Fprintf(&buf, ">>\r\n")
 	}
 
-	if len(pdfResources.images) > 0 {
+	if len(r.images) > 0 {
 		fmt.Fprintf(&buf, "/XObject << ")
-		for _, image := range pdfResources.images {
+		for _, image := range r.images {
 			fmt.Fprintf(&buf, "%v %v ", image.name, image.objectRef())
 		}
 		fmt.Fprintf(&buf, ">>\r\n")
@@ -313,32 +329,34 @@ type PdfDocument struct {
 	PdfObject
 	resources   *PdfResources
 	catalog     *PdfCatalog
-	objects     []interface{}
+	objects     []PdfObjectWriter
 	currentPage *PdfPage
+}
+
+func (d *PdfDocument) addObject(o PdfObjectWriter) {
+	o.setID(len(d.objects) + 1)
+	d.objects = append(d.objects, o)
 }
 
 // NewPdfDocument creates a new single page document
 func NewPdfDocument() PdfDocument {
-	document := PdfDocument{}
-	document.catalog = new(PdfCatalog)
-	document.catalog.objectID = nextObjectID()
-	document.catalog.pdfPages = new(PdfPages)
-	document.catalog.pdfPages.objectID = nextObjectID()
-	document.catalog.outlines = new(PdfOutlines)
-	document.catalog.outlines.objectID = nextObjectID()
-	document.resources = new(PdfResources)
-	document.resources.objectID = nextObjectID()
-	document.objects = append(document.objects, document.catalog)
-	document.objects = append(document.objects, document.catalog.pdfPages)
-	document.objects = append(document.objects, document.catalog.outlines)
-	document.objects = append(document.objects, document.resources)
-	document.addPage()
-	document.addFont("F1", 1)
-	return document
+	d := PdfDocument{}
+	d.catalog = new(PdfCatalog)
+	d.addObject(d.catalog)
+	d.catalog.pdfPages = new(PdfPages)
+	d.addObject(d.catalog.pdfPages)
+	d.catalog.outlines = new(PdfOutlines)
+	d.addObject(d.catalog.outlines)
+	d.resources = new(PdfResources)
+	d.addObject(d.resources)
+	d.addPage()
+	d.addFont("F1", 1)
+	return d
 }
 
-func (document *PdfDocument) addPage() PdfPage {
-	page := PdfPage{
+func (d *PdfDocument) addPage() PdfPage {
+	// measurements are in points
+	p := PdfPage{
 		height:       842,
 		width:        595,
 		leftMargin:   72,
@@ -347,72 +365,43 @@ func (document *PdfDocument) addPage() PdfPage {
 		bottomMargin: 72,
 		fontSize:     10,
 	}
-	page.objectID = nextObjectID()
-	page.parent = document.catalog.pdfPages
-	page.document = document
-	page.content = new(PdfPageContent)
-	page.content.text = "/F1 10 Tf\r\n1 0 0 1 72 -29 Tm\r\n10 TL\r\n"
-	page.content.graphics = "0.5 w\r\n"
-	page.content.objectID = nextObjectID()
-	document.currentPage = &page
-	document.catalog.pdfPages.pages = append(document.catalog.pdfPages.pages, page)
-	document.objects = append(document.objects, &page)
-	document.objects = append(document.objects, page.content)
-	return page
+	p.parent = d.catalog.pdfPages
+	p.document = d
+	p.content = new(PdfPageContent)
+	p.content.text = "/F1 10 Tf\r\n1 0 0 1 72 -29 Tm\r\n10 TL\r\n"
+	p.content.graphics = "0.5 w\r\n"
+	d.currentPage = &p
+	d.catalog.pdfPages.pages = append(d.catalog.pdfPages.pages, &p)
+	d.addObject(&p)
+	d.addObject(p.content)
+	return p
 }
 
-func (document *PdfDocument) addFont(name string, id int) PdfFont {
+func (d *PdfDocument) addFont(name string, id int) PdfFont {
 	font := NewFont(name, id)
-	document.resources.fonts = append(document.resources.fonts, font)
-	document.objects = append(document.objects, &font)
+	d.addObject(&font)
+	d.resources.fonts = append(d.resources.fonts, &font)
 	return font
 }
 
 // Bytes returns the byte representation of the PdfDocument
-func (document PdfDocument) Bytes() []byte {
+func (d PdfDocument) Bytes() []byte {
 	var buf bytes.Buffer
 
 	fmt.Fprintf(&buf, "%%PDF-1.2\r\n")
 	fmt.Fprintf(&buf, "%%\u00e2\u00e3\u00cf\u00d3\r\n")
 
-	xref := make([]int, len(document.objects))
+	xref := make([]int, len(d.objects))
 
-	for i, obj := range document.objects {
+	for i, obj := range d.objects {
 		xref[i] = buf.Len()
-		font, ok := obj.(*PdfFont)
-		if ok {
-			fmt.Fprintf(&buf, "%s", string(font.bytes()))
-		}
-		pages, ok := obj.(*PdfPages)
-		if ok {
-			fmt.Fprintf(&buf, "%s", string(pages.bytes()))
-		}
-		page, ok := obj.(*PdfPage)
-		if ok {
-			fmt.Fprintf(&buf, "%s", string(page.bytes()))
-		}
-		content, ok := obj.(*PdfPageContent)
-		if ok {
-			fmt.Fprintf(&buf, "%s", string(content.bytes()))
-		}
-		catalog, ok := obj.(*PdfCatalog)
-		if ok {
-			fmt.Fprintf(&buf, "%s", string(catalog.bytes()))
-		}
-		resources, ok := obj.(*PdfResources)
-		if ok {
-			fmt.Fprintf(&buf, "%s", string(resources.bytes()))
-		}
-		outlines, ok := obj.(*PdfOutlines)
-		if ok {
-			fmt.Fprintf(&buf, "%s", string(outlines.bytes()))
-		}
+		fmt.Fprintf(&buf, "%s", obj.bytes())
 	}
 
 	startxref := buf.Len()
 
 	fmt.Fprintf(&buf, "xref\r\n")
-	fmt.Fprintf(&buf, "0 %v \r\n", len(document.objects)+1)
+	fmt.Fprintf(&buf, "0 %v \r\n", len(d.objects)+1)
 	fmt.Fprintf(&buf, "0000000000 65535 f\r\n")
 	for i := range xref {
 		fmt.Fprintf(&buf, "%010d 00000 n\r\n", xref[i])
@@ -420,7 +409,7 @@ func (document PdfDocument) Bytes() []byte {
 	fmt.Fprintf(&buf, "trailer\r\n")
 	fmt.Fprintf(&buf, "<<\r\n")
 	fmt.Fprintf(&buf, "/Size %v\r\n", len(xref))
-	fmt.Fprintf(&buf, "/Root %v\r\n", document.catalog.objectRef())
+	fmt.Fprintf(&buf, "/Root %v\r\n", d.catalog.objectRef())
 	fmt.Fprintf(&buf, ">> \r\n")
 	fmt.Fprintf(&buf, "startxref\r\n")
 	fmt.Fprintf(&buf, "%v\r\n", startxref)
@@ -432,7 +421,8 @@ func (document PdfDocument) Bytes() []byte {
 // Test
 func main() {
 	document := NewPdfDocument()
-	document.currentPage.outputText("Hello World")
+	document.currentPage.println("Hello \\(World)")
+	document.currentPage.println("Goodbye World")
 	fmt.Printf("%v\n", string(document.Bytes()))
 
 }
